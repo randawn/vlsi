ccfd08834f6f52388b8603c7f076493fe95fb3b0
randawn <saviouran@gmail.com>
2014-04-13 10:24:03 +0800

    initial check in

---
+++ placer/placer.py
@@ -0,0 +1,22 @@
+
+class NetList(object):
+    def __init__():
+        gate = {num:0, nets:[]}
+        pad = {wire:0, }
+
+
+
+    def InputParser():
+        fh = open('toy1')
+        # first line: two int G: num of Gates N:num of nets
+        first_line = fh.readline()
+        for line_cnt in range(gate_num):
+            line = fh.readline()
+            add_gate(gate, nets)
+        #next line: one int P: num of pad P
+        next_line = fh.readline()
+        for line_cnt in range(pad_num):
+            line = fh.readline()
+            add_pad(net, X, Y)
+    def OutputGen():
+        pass
