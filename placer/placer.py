32a7de9977d221cd9089ee5e58a1c3f756920c1e
randawn <saviouran@gmail.com>
2014-04-17 01:43:09 +0800

    update

--- placer/placer.py
+++ placer/placer.py
@@ -15,13 +15,13 @@ class NetList(object):
         #self.x = []
         #self.y = []
     def PrintGates(self, gates):
-        for ID, gate in gates.items():
-            print ID
-            print gate
+        for ID in sorted([ ID for ID in gates.keys()], key=lambda ID: int(ID)):
+            print 'gate'+ID
+            print gates[ID]
     def PrintPads(self, pads):
-        for ID, pad in pads.items():
-            print ID
-            print pad
+        for ID in sorted([ ID for ID in pads.keys()], key=lambda ID: int(ID)):
+            print 'pad'+ID
+            print pads[ID]
     def Place(self):
         gates = self.gates
         pads = self.pads
@@ -87,24 +87,17 @@ class NetList(object):
             pads['P'+pad_ID] = pad
         return pads
 
-    def DisplayGate(self):
-        for gate in self.gate_lst:
-            print gate
-
-    def DisplayPad(self):
-        for pad in self.pad_lst:
-            print pad
-
     def InputParser(self):
         '''get the pads and gates from file'''
-        # gates            = {'ID':{'num':num, 'nets':[(wire, weight)], 'coord':(X, Y)}}
-        # pads(in file)    = {'ID':{'wire':wire,      'coord':(X, Y)}}
-        # pads(after Kto2) = {'ID':{'gates':[gate_ID], 'coord':(X, Y)}}
-        gates = {}
-        pads = {}
+        # gates(in file)    = {'ID':{'coord':(X, Y), 'nets':[(wire, weight)]        }}
+        # gates(after Kto2) = {'ID':{'coord':(X, Y), 'gates':[(gate_ID, weight)],  'pads':[(pad_ID, weight)], 'num':num}}
+        # pads(in file)     = {'ID':{'coord':(X, Y), 'nets':[(wire, weight)]        }}
+        # pads(after Kto2)  = {'ID':{'coord':(X, Y), 'gates':[(gate_ID, weight)]    }}
+        gatesk = {}
+        padsk = {}
         fh = open('toy1')   # no comment for parsing!!!
         pattern0 = re.compile("(\d+) (\d+)")
-        pattern1 = re.compile("(\d+)")  #TODO: we do know how many of \d+
+        pattern1 = re.compile("(\d+)")                      #TODO: we do know how many of \d+
         pattern2 = re.compile("(\d+)")
         pattern3 = re.compile("(\d+) (\d+) (\d+) (\d+)")
         first_line = fh.readline()
@@ -121,7 +114,7 @@ class NetList(object):
                     gate['nets'].append((all_lst[2+i], 1))
                 except KeyError as e:
                     gate['nets'] = [(all_lst[2+i], 1)]
-            gates[gate_ID] = gate
+            gatesk[gate_ID] = gate
         #next line: one int P: num of pad P
         next_line = fh.readline()
         match = pattern2.search(next_line)
@@ -132,44 +125,52 @@ class NetList(object):
             match = pattern3.search(line)
             (pin_ID, net_num, X, Y) = match.groups()
             pad_ID = pin_ID
-            pad['wire'] = net_num
+            pad['nets'] = [(net_num, 1)]
             pad['coord'] = (int(X), int(Y))
-            pads[pad_ID] = pad
+            padsk[pad_ID] = pad
         fh.close()
-        self.KTo2(int(net_sum), gates, pads)
+        self.KTo2(int(net_sum), gatesk, padsk)
 
-    def KTo2(self, net_sum, gates, pads):
+    def KTo2(self, net_sum, gatesk, padsk):
         '''trans k-point netlist to 2-point'''
-        #trans pads first
-        for pad_ID, pad in pads.items():
-            pad['gates'] = []
-            for gate_ID, gate in gates.items():
-                for wire, weight in gate['nets']:
-                    if (pad['wire'] == wire):# this gate and pad is connected
-                        pad['gates'].append(gate_ID)
-            del pad['wire']
+        gates = copy.deepcopy(gatesk)
+        pads  = copy.deepcopy(padsk)
         # now trans k-point netlist to 2-point
         # decompose the wire connected with more the 2 gates
-        for i in range(1,net_sum+1):
+        for i in range(1, net_sum+1):
             gate_rcd = []
+            pad_rcd = []
             net_rcd = []
-            for ID, gate in gates.items():
+            for gate_ID, gate in gatesk.items():
                 for net_name, weight in gate['nets']:
-                    if (str(i) == net_name):
-                        gate_rcd.append(ID)
+                    if (net_name == str(i)):
+                        gate_rcd.append(gate_ID)
                         net_rcd.append(net_name)
-            if (len(gate_rcd) > 2):
-                weight = 1.0/(len(gate_rcd)-1)
+            for pad_ID, pad in padsk.items():
+                if (pad['nets'][0][0] == str(i)):
+                    pad_rcd.append(pad_ID)
+            point_cnt = len(gate_rcd) + len(pad_rcd)
+            if (len(pad_rcd)>1):
+                raise PadtoPadconnetion
+            if (point_cnt > 2):
+                weight = 1.0/(point_cnt-1)
                 # add new 
                 for (id0, id1) in list(combinations(gate_rcd, 2)):
                     net_name = str(id0)+'_'+str(id1)+'decompose'
                     gates[id0]['nets'].append((net_name, weight))
                     gates[id1]['nets'].append((net_name, weight))
+                if (len(pad_rcd)>0):
+                    for gate_ID in gate_rcd:
+                        net_name = str(gate_ID)+'_pad'+str(pad_rcd[0])+'decompose'
+                        gates[gate_ID]['nets'].append((net_name, weight))
+                        pads[pad_rcd[0]]['nets'].append((net_name, weight))
                 # remove old
                 for i in range(len(gate_rcd)):
                     ID = gate_rcd[i]
                     net_name = net_rcd[i]
                     gates[ID]['nets'].remove((net_name, 1))
+                if (len(pad_rcd)>0):
+                    pads[pad_rcd[0]]['nets'].append((net_name, 1))
         # merge if there are more than 1 wire connect two gates 
         for ID, ID_ex in list(combinations(gates.keys(), 2)):
             net_rcd = []
@@ -192,6 +193,51 @@ class NetList(object):
                         gates[ID_ex]['nets'].remove((net_rcd[i], weight_rcd[i]))
                     except ValueError as e:
                         pass
+        # now trans gate['nets'] to gate['gates']
+        for ID, ID_ex in list(combinations(gates.keys(), 2)):
+            for wire, weight in gates[ID]['nets']:
+                for wire_ex, weight_ex in gates[ID_ex]['nets']:
+                    if (wire == wire_ex):
+                        try:
+                            gates[ID]['gates'].append((ID_ex, weight))
+                            gates[ID_ex]['gates'].append((ID, weight))
+                        except KeyError as e:
+                            gates[ID]['gates']      = [(ID_ex, weight)]
+                            gates[ID_ex]['gates']   = [(ID, weight)]
+        # and gate['pads']
+        # trans pads 
+        for pad_ID, pad in pads.items():
+            for wire_p, weight_p in pad['nets']:
+                for gate_ID, gate in gates.items():
+                    for wire, weight in gate['nets']:
+                        if (pad_ID == '4' and gate_ID == '2'):
+                            print '===========>'
+                            print pad
+                            print gate
+                            print wire
+                            print wire_p
+                        if (wire_p == wire):       # this gate and pad is connected
+                            if (pad_ID == '4' and gate_ID == '2'):
+                                print '===========>'
+                                print pad
+                                print gate
+                            try:
+                                pad['gates'].append((gate_ID, weight))
+                                gate['pads'].append((pad_ID, weight))
+                            except KeyError as e:
+                                pad['gates']   = [(gate_ID, weight)]
+                                gate['pads']   = [(pad_ID, weight)]
+                            if (pad_ID == '4' and gate_ID == '2'):
+                                print '===========>'
+                                print pad
+                                print gate
+        # remove nets
+        for pad_ID, pad in pads.items():
+            del pad['nets']
+        for gate_ID, gate in gates.items():
+            del gate['nets']
+        self.PrintGates(gates)
+        self.PrintPads(pads)
         self.gates = gates
         self.pads = pads
 
@@ -210,12 +256,12 @@ class NetList(object):
         for i in range(len(IDseq)):
             gates[IDseq[i]]['num'] = i
         for ID, gate in gates.items():
-            for net, weight in gate['nets']:
+            for id, weight in gate['gates']:
                 for ID_ex, gate_ex in gates.items():
                     if (ID == ID_ex):
                         continue
-                    for net_ex, weight_ex in gate_ex['nets']:
-                        if (net == net_ex):
+                    for id_ex, weight_ex in gate_ex['gates']:
+                        if (id == id_ex):
                             R_lst.append(gate['num'])
                             C_lst.append(gate_ex['num'])
                             V_lst.append(weight)
@@ -231,8 +277,9 @@ class NetList(object):
                 if (row == R_lst[i]):
                     r_sum += V_lst[i]
             for padID, pad in pads.items():# add pad if there is 
-                if (str(row+1) in pad['gates']):
-                    r_sum += 1.0
+                for id, weight in pad['gates']:
+                    if (str(row+1) == id):
+                        r_sum += weight
             R_lst.append(row)
             C_lst.append(row)
             V_lst_n.append(r_sum)
@@ -244,20 +291,26 @@ class NetList(object):
     def GetVecb(self, gates, pads):
         bx = [0]*len(gates)
         by = [0]*len(gates)
-        print '==============='
-        #self.PrintGates(gates)
-        #self.PrintPads(pads)
         for pad_ID, pad in pads.items():
             for gate_ID, gate in gates.items():
-                if (gate_ID in pad['gates']):
-                    bx[gate['num']] += pad['coord'][0]
-                    by[gate['num']] += pad['coord'][1]
-                    #by[int(gate_ID)-1] += pad['coord'][1]
+                for id, weight in pad['gates']:
+                    if (gate_ID == id):
+                        bx[gate['num']] += weight*pad['coord'][0]
+                        by[gate['num']] += weight*pad['coord'][1]
+                        #by[int(gate_ID)-1] += pad['coord'][1]
+        print bx
+        print by
+
         return (array(bx), array(by))
 
     def SolveMat(self, gates, pads):
         (R, C, V) = self.GetMatA(gates, pads)
         (bx, by) = self.GetVecb(gates, pads)
+        print R
+        print C
+        print V
+        print bx
+        print by
         A = coo_matrix((V, (R, C)), shape=(len(bx), len(bx)))
         # convert to csr format for efficiency
         x = spsolve(A.tocsr(), bx)
@@ -269,7 +322,5 @@ class NetList(object):
 if __name__ == '__main__':
     my_netlist = NetList()
     my_netlist.InputParser()
-    #my_netlist.DisplayGate()
-    #my_netlist.DisplayPad()
     my_netlist.Place()
 
